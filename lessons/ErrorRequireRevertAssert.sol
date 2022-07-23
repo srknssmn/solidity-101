6596b30a2907e664c14695ba62f2d25bd033801c
"Serkan Şişman" <"srknssmn@gmail.com">
2022-07-23 23:09:27 +0300

    update

---
+++ lessons/ErrorRequireRevertAssert.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract ErrorTest {
+
+    function testRequire(uint _x) public pure {
+        require (_x<=10, "_x greater than 10" );  // condition true if _x smaller than 10, if not say: _x greater than 10
+    }
+    function testRevert(uint _x) public pure {
+        if(_x>10){
+            revert ("_x greater than 10");
+        } else if (_x>20){
+            revert ("_x greater than 20");
+        }else{
+        }
+    }
+    
+        uint public num=123; // Değişmesini istemiyoruz.
+        function numFun() public view {
+            assert (num==123);
+        }
+        function ornek() public {
+            num+=1;
+ // num assert'ten dolayı değişmemesi gerektiği için; numFun artık call edildiğinde ERROR mesajı alınır!                         
+        }
+    }
